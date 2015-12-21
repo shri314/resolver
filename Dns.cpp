56c9610a2f1da5a9a0bfa0f769bd7ecf5bd127e2
Shriram V <shri314@yahoo.com>
2015-12-21 17:12:58 +0530

    Added build.sh, initial DNS Header class

--- Dns.cpp
+++ Dns.cpp
@@ -1,6 +1,21 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
+
+struct alignas(2) DnsHeader
+{
+   uint16_t ID;
+   uint16_t Flags;
+   uint16_t QDCount;
+   uint16_t AnCount;
+   uint16_t NsCount;
+   uint16_t ArCount;
+};
+
+
+
+
+
 namespace asio = boost::asio;
 typedef asio::ip::tcp protocol;
 typedef protocol::resolver resolver;
@@ -19,6 +34,9 @@ void handle_resolve_query(const error_code& ec, resolver::iterator iter)
    }
 }
 
+#include <iostream>
+using std::cout;
+
 int main(int argc, char** argv)
 {
    asio::io_service io_service;
@@ -30,6 +48,8 @@ int main(int argc, char** argv)
    }
 
    io_service.run();
+
+   cout << sizeof(DnsHeader) << "\n";
 
    return 0;
 }
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -std=c++1z -o Dns Dns.cpp -lboost_system -lboost_thread -lpthread
